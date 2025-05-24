a551de489752c14d844a219da3d55ac1f34ecc30
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-25 04:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn=document.getElementByClass(".btn");
+let div=document.getElementByClass(".search");
+let input=document.getElementByClass(".input");
+btn.addEventListener("click",()=>{
+	div.classList("active");
+	input.focus();
+	
+}
+)
